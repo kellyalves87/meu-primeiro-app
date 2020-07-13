b5fb8540366f072845227142e8b2b58c09d5e094
Kelly Alves <kellyp.alves87@gmail.com>
2020-07-13 10:47:44 -0300

    First commit

---
+++ src/components/Progress/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Progress = () => {
+    return(<p>Oi sou o Progress</p>)
+}
+
+export default Progress;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+    return(<p>Oi sou a Home</p>)
+}
+
+export default Home;
