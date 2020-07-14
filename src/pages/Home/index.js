6b880550e8a998bc424c0ff0dbc02544faa1e652
Kelly Alves <kellyp.alves87@gmail.com>
2020-07-14 13:02:16 -0300

    developing home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,25 @@
 import React from "react";
 
+import Progress from "../../components/Progress";
+
 const Home = () => {
-    return(<p>Oi sou a Home</p>)
-}
+  return (
+    <article>
+      <p>
+        Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam
+        a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Tá deprimidis,
+        eu conheço uma cachacis que pode alegrar sua vidis. Interessantiss
+        quisso pudia ce receita de bolis, mais bolis eu num gostis. Nec orci
+        ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum
+        felis.
+      </p>
+      <div className="App__content__subs">
+        <p className="App__content__subs__count">133</p>
+        <p className="App__content__subs__subtitle">Inscritos</p>
+      </div>
+      <Progress />
+    </article>
+  );
+};
 
 export default Home;
